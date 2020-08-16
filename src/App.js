cd8f9e1f151ba124a8bbd3fdf77b72c5ffaed2b3
itsamanverma <amanvermame786@gmail.com>
2020-08-16 21:27:33 +0530

    create the service worker file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
-import Navigation from './components/Navigation/Navigation';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Users from './components/Users/Users';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Navigation />
+      <Router>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="users">Users</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/about">About</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route path="/about" component={About}></Route>
+          <Route path="users" component={Users}></Route>
+          <Route path="/" component={Home}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
